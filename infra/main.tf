868a30b950f115e3a18441c652a3e7bf89d9f30e
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-05-07 15:40:59 -0300

    fix: infra to dynamodb

--- infra/main.tf
+++ infra/main.tf
@@ -15,7 +15,6 @@ resource "aws_dynamodb_table" "notifications" {
   name = "notifications"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
-  range_key = "user_id"
 
   attribute {
     name = "id"
@@ -27,50 +26,8 @@ resource "aws_dynamodb_table" "notifications" {
     type = "S"
   }
 
-  attribute {
-    name = "title"
-    type = "S"
-  }
-
-  attribute {
-    name = "content"
-    type = "S"
-  }
-
-  attribute {
-    name = "read"
-    type = "B"
-  }
-
   global_secondary_index {
-    name               = "GSI1"
-    hash_key           = "user_id"
-    range_key          = "content"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "ALL"
-  }
-
-   global_secondary_index {
-    name               = "TitleIndex"
-    hash_key           = "title"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "ALL"
-  }
-
-  # Índice global secundário para consulta por leitura
-  global_secondary_index {
-    name               = "ReadIndex"
-    hash_key           = "read"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "ALL"
-  }
-
-  # Índice global secundário para consulta por usuário
-  global_secondary_index {
-    name               = "UserIdIndex"
+    name               = "userId_index"
     hash_key           = "user_id"
     write_capacity     = 1
     read_capacity      = 1
